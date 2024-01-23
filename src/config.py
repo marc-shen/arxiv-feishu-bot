01ce2185c9780d884a14d90ccfcb54918ad80095
Marc Shen <marcshen@163.com>
2024-01-23 16:57:42 +0800

    更新了天文相关的关键词

--- src/config.py
+++ src/config.py
@@ -2,11 +2,79 @@
 # Other options: 'https://arxiv.org/list/cs/new',
 #                'https://arxiv.org/list/cs.AI/new'
 #                'https://arxiv.org/list/cs.CV/new'
-NEW_SUB_URL = 'https://arxiv.org/list/cs.CL/new'
+NEW_SUB_URL = "https://arxiv.org/list/astro-ph.HE/new"
 
 # Keywords to search
-KEYWORD_LIST = ["sentiment analysis", "aspect based", "augmentation"]
+KEYWORD_LIST = [
+    "GRB",
+    "FRB",
+    "afterglow",
+    "supernova",
+    "GW",
+    "gravitational wave",
+    "X-ray",
+    "radio",
+    "optical",
+    "gamma-ray",
+    "neutrino",
+    "cosmic ray",
+    "magnetar",
+    "magnetism",
+    "magnetic field",
+    "magnetic reconnection",
+    "magnetic flux",
+    "magnetic energy",
+    "magnetic turbulence",
+    "magnetic field amplification",
+    "magnetic field generation",
+    "magnetic field evolution",
+    "magnetic field decay",
+    "magnetic field dissipation",
+    "magnetic field annihilation",
+    "magnetic field reconnection",
+    "magnetic field turbulence",
+    "magnetic field amplification",
+    "magnetic field generation",
+    "magnetic field evolution",
+    "magnetic field decay",
+    "magnetic field dissipation",
+    "magnetic field annihilation",
+    "magnetic field reconnection",
+    "magnetic field turbulence",
+    "magnetic field amplification",
+    "magnetic field generation",
+    "magnetic field evolution",
+    "magnetic field decay",
+    "magnetic field dissipation",
+    "magnetic field annihilation",
+    "magnetic field reconnection",
+    "magnetic field turbulence",
+    "magnetic field amplification",
+    "magnetic field generation",
+    "magnetic field evolution",
+    "magnetic field decay",
+    "magnetic field dissipation",
+    "magnetic field annihilation",
+    "magnetic field reconnection",
+    "magnetic field turbulence",
+    "magnetic field amplification",
+    "magnetic field generation",
+    "magnetic field evolution",
+    "magnetic field decay",
+    "magnetic field dissipation",
+    "magnetic field annihilation",
+    "magnetic field reconnection",
+    "magnetic field turbulence",
+]
+
 
 # WEBHOOK_URL in feishu bot, Please replace the fake URL and secret with yours
-WEBHOOK_URL = "https://open.feishu.cn/open-apis/bot/v2/hook/#"
-WEBHOOK_SECRET = "#"
+# WEBHOOK_URL = (
+#     "https://open.feishu.cn/open-apis/bot/v2/hook/2cc640aa-2e6a-44bc-a7d4-99048f44402d"
+# )
+# WEBHOOK_SECRET = "T5sydBsunbWbFmcr1DZxrd"
+
+WEBHOOK_URL = (
+    "https://open.feishu.cn/open-apis/bot/v2/hook/8330ea90-10ce-4cdc-b511-1e9241e42adb"
+)
+WEBHOOK_SECRET = "Yuxbt97J4SJTrcu8PRZNIb"
